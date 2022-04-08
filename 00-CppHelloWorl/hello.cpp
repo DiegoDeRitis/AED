b5579bf5b3f8af67911678fc42d4cc33b4876699
DiegoDeRitis <103281472+DiegoDeRitis@users.noreply.github.com>
2022-04-08 16:50:05 -0300

    Add files via upload

---
+++ 00-CppHelloWorl/hello.cpp
@@ -0,0 +1,14 @@
+//
+//  hello.cpp
+// Programa destinado a comprobar las capacidades a la hora de crear un programa simple
+// Diego De Ritis
+//08/04/2022
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
